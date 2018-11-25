b4ce53be48ff0ad7708034c1b4067291a2d713b1
joy <13164432553@163.com>
2018-11-25 12:39:13 +0800

    init project

---
+++ src/main/java/org/ljk/zodiac/cow/form/engine/CowFormEngineApplication.java
@@ -0,0 +1,12 @@
+package org.ljk.zodiac.cow.form.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowFormEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CowFormEngineApplication.class, args);
+    }
+}
